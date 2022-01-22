bad52d93e9236510828d0ea319ed3513c96f5768
llqishi <1090327127@qq.com>
2022-01-22 13:31:54 +0800

    v1.2
    新增了几关

---
+++ princess/Assets/脚本/UIbutton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIbutton : MonoBehaviour
+{
+    enum enum_buttonMode {start,exit }
+
+    [SerializeField]enum_buttonMode ButtonMode;
+
+    [SerializeField]
+    GameObject ui;
+
+    void Awake()
+    {
+        ui.SetActive(false);
+    }
+
+    void OnMouseEnter()
+    {
+       ui.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        ui.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        if(ButtonMode==enum_buttonMode.start)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if(ButtonMode == enum_buttonMode.exit)
+        {
+            Application.Quit(); 
+        }
+    }
+}
